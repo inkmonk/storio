0cd3a668710e0a24874f60b32576263e228c58f3
Sibi <sibi@psibi.in>
2015-05-09 16:23:22 +0530

    Prototype expanded.

--- website/static/storify.js
+++ website/static/storify.js
@@ -1,9 +1,19 @@
 angular.module('storifyApp', ['btford.socket-io'])
     .factory('mySocket', function(socketFactory) {
-        return socketFactory();
+        var myIoSocket = io.connect('http://localhost.com:5000');
+
+        var mySocket = socketFactory({
+            ioSocket: myIoSocket
+        });
+
+        return mySocket;
     })
-    .controller('storifyController', function($scope) {
+    .controller('storifyController', function($scope, mySocket) {
         console.log('hell world');
         $scope.test = "hello world";
+
+        mySocket.emit('connect', function(something) {
+            console.log('some', something);
+        });
     });
 
