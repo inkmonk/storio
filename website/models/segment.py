68f782700333cfe458056e69cd13918e683a13a9
SuryaSankar <suryashankar.m@gmail.com>
2015-05-10 00:47:16 +0530

    commit

--- website/models/segment.py
+++ website/models/segment.py
@@ -6,23 +6,22 @@ class Segment(db.Model):
 
     __tablename__ = 'segment'
 
-    _attrs_to_serialize_ = ['id', 'story_id', 'created_at']
+    _attrs_to_serialize_ = ['id', 'story_id', 'created_at', 'first_snippet']
 
-    _rels_to_expand_ = ['snippets', 'first_snippet']
+    _rels_to_expand_ = ['snippets']
 
     id = db.Column(db.Integer, primary_key=True, unique=True)
     story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
     created_at = db.Column(db.DateTime())
 
     story = db.relationship("Story")
-    snippets = db.relationship("Snippet")
+    snippets = db.relationship("Snippet", order_by="Snippet.created_at")
 
     @property
     def first_snippet(self):
-        for snippet in self.snippets:
-            if snippet.is_first:
-                return snippet
-        return None
+        if len(self.snippets) == 0:
+            return None
+        return self.snippets[0]
 
     def __init__(self, story_id=None):
         self.story_id = story_id
