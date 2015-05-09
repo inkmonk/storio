8bca3cde258fb8d19c204a00291acb808c0b1037
Sibi <sibi@psibi.in>
2015-05-09 15:32:25 +0530

    Add storify bp.

--- website/views/pages.py
+++ website/views/pages.py
@@ -6,3 +6,7 @@ pages_bp = Blueprint('standalone_pages_bp', __name__)
 @pages_bp.route('/')
 def home():
     return render_template('index.html')
+
+@pages_bp.route('/storify')
+def storify():
+    return render_template('storify.html')
