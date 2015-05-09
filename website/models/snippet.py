fa6ca5a64740583ee7415bfed7164b2222d0119a
SuryaSankar <suryashankar.m@gmail.com>
2015-05-09 17:44:03 +0530

    adding stories

--- website/models/snippet.py
+++ website/models/snippet.py
@@ -10,10 +10,20 @@ class Snippet(db.Model):
     text = db.Column(db.UnicodeText)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime())
+    story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
 
     user = db.relationship("User")
+    story = db.relationship("Story")
 
-    def __init__(self, text=None, user_id=None):
+    def __init__(self, text=None, user_id=None, user=None,
+                 story_id=None, story=None):
         self.text = text
-        self.user_id = user_id
+        if user_id:
+            self.user_id = user_id
+        elif user:
+            self.user = user
         self.created_at = ist_now()
+        if story_id:
+            self.story_id = story_id
+        elif story:
+            self.story = story
