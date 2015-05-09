1994b56f7a0f01f7d0d96e250ac7aa5b2db4b33d
SuryaSankar <suryashankar.m@gmail.com>
2015-05-09 22:37:49 +0530

    document events

--- website/events_controller.py
+++ website/events_controller.py
@@ -3,12 +3,12 @@ socketio = SocketIO()
 from flask.ext.socketio import emit, send, join_room, leave_room
 from .authenticators import authenticated_socket
 from flask.ext.security import current_user
-from .models import Segment, Snippet
+from .models import Segment, Snippet, Story
 
 """
 EVENTS:
-    
-    CLIENT SENDS:
+
+    CLIENT EMITS:
 
     join
     ----
@@ -16,7 +16,15 @@ EVENTS:
         'story_id': 1
     }
 
-    SERVER EMITS:
+    SERVER EMITS
+
+    welcome
+    -------
+    {
+        'current_segment_id': 12
+    }
+
+    SERVER BROADCASTS:
 
     user_joined
     -----------
@@ -24,7 +32,7 @@ EVENTS:
         'user': 'sibi'
     }
 
-    CLIENT SENDS:
+    CLIENT EMITS:
 
     leave
     -----
@@ -32,13 +40,78 @@ EVENTS:
         'story_id': 1
     }
 
-    SERVER EMITS:
+    SERVER BROADCASTS:
 
     user_left
     -----------
     {
         'user': 'sibi'
     }
+
+    CLIENT EMITS:
+
+    modify_snippet_text
+    -------------------
+
+    {
+        'story_id': 1,
+        'segment_id': 13,
+        'text': 'Hello worl'
+    }
+
+    SERVER BROADCASTS:
+
+    user_modified_snippet_text
+    --------------------------
+    {
+       'user': 'sibi',
+       'text': 'Hello worl',
+       'segment_id': 13
+    }
+
+    CLIENT EMITS:
+
+    submit_snippet
+    --------------
+    {
+        'story_id': 1,
+        'segment_id': 13,
+        'text': 'Hello world. I finished my snippet'
+    }
+
+    SERVER BROADCASTS:
+
+    handover_snippet_and_start_next_segment
+    ---------------------------------------
+    {
+        'segment_id': 13,
+        'first_snippet': {
+            'segment_id': 13,
+            'text': 'Hello world. I finished my snippet',
+            'is_first': true,
+            'user': 'sibi'
+        },
+        'next_segment_id': 14
+
+    }
+
+    SERVER BROADCASTS
+
+    append_snippet
+    ---------------
+    {
+        'segment_id': 13,
+        'snippet': {
+            'segment_id': 13,
+            'text': 'Hello world. i am slow',
+            'is_first': false,
+            'user': 'surya'
+        }
+
+    }
+
+
+
 
 """
 
@@ -47,8 +120,16 @@ EVENTS:
 @authenticated_socket
 def on_join(data):
     story_id = data['story_id']
+    current_segment = Segment.last(story_id=story_id)
+    if current_segment is None:
+        current_segment = Segment.create(story_id=story_id)
     join_room(story_id)
-    emit('user_joined', {'user': current_user.email}, room=story_id)
+    emit('welcome', {
+        'current_segment_id': current_segment.id
+        }, room=story_id)
+    emit('user_joined', {
+        'user': current_user.email,
+        }, broadcast=True, room=story_id)
 
 
 @socketio.on('leave')
@@ -57,7 +138,8 @@ def on_leave(data):
     story_id = data['story_id']
     leave_room(story_id)
     send(current_user.email + ' has left the room.', room=story_id)
-    emit('user_left', {'user': current_user.email}, room=story_id)
+    emit('user_left', {'user': current_user.email},
+         room=story_id, broadcast=True)
 
 
 @socketio.on('modify_snippet_text')
@@ -67,27 +149,31 @@ def on_modify_snippet_text(data):
         'user': current_user.email,
         'text': data['text'],
         'segment_id': data['segment_id']
-        }, room=data['story_id'])
+        }, room=data['story_id'], broadcast=True)
 
 
 @socketio.on('submit_snippet')
 @authenticated_socket
 def on_submit_snippet(data):
     segment_id = data['segment_id']
+    story_id = data['story_id']
     text = data['text']
     segment = Segment.get(segment_id)
     if len(segment.snippets) == 0:
         snippet = Snippet.create(
             segment_id=segment_id,
             text=text, is_first=True)
-        emit('handover_snippet', {
+        next_segment = Segment.create(story_id)
+        emit('handover_snippet_and_start_next_segment', {
             'segment_id': segment_id,
             'first_snippet': snippet.todict(),
-            })
+            'next_segment_id': next_segment.id
+            }, broadcast=True, room=data['story_id'])
     else:
         snippet = Snippet.create(
             segment_id=segment_id,
             text=text)
         emit('append_snippet', {
-            'segment_id': segment_id
-            })
+            'segment_id': segment_id,
+            'snippet': snippet.todict()
+            }, broadcast=True, room=data['story_id'])
